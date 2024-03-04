917ab622450a2dc984d5e9cc07e1126284461a03
greenmeansgo1012 <greenmeansg@proton.me>
2024-03-05 05:47:19 +0900

    fixing the hosting addreess

--- src/pages/Authentication/LoginBoxed.tsx
+++ src/pages/Authentication/LoginBoxed.tsx
@@ -30,7 +30,7 @@ const LoginBoxed = () => {
         navigate('/');
     };
     const authTest = (provider: any) => {
-        return window.location.href = `http://localhost:8000/api/v1/auth/${provider}/login`
+        return window.location.href = `https://api.uncappedtheory.com/api/v1/auth/${provider}/login`
     }
 
     return (
